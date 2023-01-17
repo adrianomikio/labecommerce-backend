4cc4fa782bc913b51e953a08bd5d3f48d9f22e28
Adriano Mikio <adrianouge@gmail.com>
2023-01-17 14:25:16 -0300

    feat - CRUD das tabelas

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -57,3 +57,34 @@ VALUES (
         5,
         "Ervas e Especiarias"
     );
+
+SELECT * FROM users;
+
+-- Refatoração do ex. 3 -> ORDER BY email ASC;
+
+SELECT * FROM products ORDER BY price ASC LIMIT 20;
+
+SELECT * FROM products WHERE name LIKE "Chá%";
+
+INSERT INTO
+    users(id, email, password)
+VALUES (
+        "04",
+        "rosana@email.com",
+        "senhaDaRosana"
+    );
+
+SELECT * FROM products WHERE id = "01";
+
+DELETE FROM users WHERE id = "03";
+
+DELETE FROM products WHERE id = "02";
+
+UPDATE users SET email = "adriano@email.com.br" WHERE id = "01";
+
+UPDATE products SET price = 6.5 WHERE id = "01";
+
+SELECT *
+FROM products
+WHERE price > 3 AND price <= 5
+ORDER BY price ASC;
